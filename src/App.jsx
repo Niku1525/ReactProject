15e7d59cf4fbdf1062107d7ac7cf16278df76685
Niku <armandosempai10@gmail.com>
2025-12-28 03:39:18 -0500

    Revert "FINISH!"
    This reverts commit 83ff25a1f16d30ff2c624b7466ec4aca695cc066.

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,11 @@ function App() {
     <>
       <Container
         title="¡Feliz Cumpleaños Wsiswi!"
-        text={
-          <>
-            ¡Te deseo lo mejor en tu día especial, porque eres el mejor! <br />
-            Gracias por ser esa persona con la que siempre se puede contar,
-            <br /> para reír, para soplar, para odiar peruanos, para alabar a
-            Eika,
-            <br /> para disque jugar repo o simplemente para pasar el rato.
-            <br />
-            ¡Espero que cumplas todas tus metas chaval!
-            <br />
-            Con mucho cariño, Niku✨
-          </>
-        }
+        text="¡Te deseo lo mejor en tu día especial!"
         onButtonClick={() => setMostrarVideo(!mostrarVideo)}
         btnText={mostrarVideo}
       />
+
       {mostrarVideo && (
         <div className="video-container">
           <Video srcVideo="./assets/Korone_Test.mp4" videoClass="video" />
